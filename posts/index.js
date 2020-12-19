6ce763bcaa620925a5ab7d10f4804f0bdc4084d2
pie <returnpie@icloud.com>
2020-12-19 18:27:39 +0900

    node port

--- posts/index.js
+++ posts/index.js
@@ -15,6 +15,7 @@ app.get("/posts", (req, res) => {
 });
 
 app.post("/posts", async (req, res) => {
+  console.log("??");
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
@@ -22,7 +23,7 @@ app.post("/posts", async (req, res) => {
     id,
     title,
   };
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
@@ -40,5 +41,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("v55");
   console.log("Listening on 4000");
 });
